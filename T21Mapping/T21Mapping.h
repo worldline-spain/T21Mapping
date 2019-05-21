42c5fb8ca4217f3c049b9914a76a167b7a98deed
Marcos Molero <marcos.molero@worldline.com>
2019-05-21 16:07:14 +0200

    Added Carthage compatibility

---
+++ T21Mapping/T21Mapping.h
@@ -0,0 +1,19 @@
+//
+//  T21Mapping.h
+//  T21Mapping
+//
+//  Created by Marcos Molero on 21/05/2019.
+//  Copyright © 2019 Worldline. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for T21Mapping.
+FOUNDATION_EXPORT double T21MappingVersionNumber;
+
+//! Project version string for T21Mapping.
+FOUNDATION_EXPORT const unsigned char T21MappingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <T21Mapping/PublicHeader.h>
+
+
